214ad555faafd513c8453bd11e0d9ffe01b3cb28
Beckham07m <as.atsadayut.gmail.com>
2024-06-12 23:39:48 +0700

    first commit

---
+++ src/Nav.jsx
@@ -0,0 +1,32 @@
+import { Container } from "@chakra-ui/react";
+import { Button, Stack } from "@chakra-ui/react";
+
+const Nav = () => {
+  return (
+    <div>
+      <Container>
+        There are many benefits to a joint design and development system. Not
+        only does it bring benefits to the design team, but it also brings
+        benefits to engineering teams. It makes sure that our experiences have a
+        consistent look and feel, not just in our design specs, but in
+        production
+      </Container>
+      <Stack spacing={4} direction="row" align="center">
+        <Button colorScheme="teal" size="xs">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="sm">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="md">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="lg">
+          Button
+        </Button>
+      </Stack>
+    </div>
+  );
+};
+
+export default Nav;
